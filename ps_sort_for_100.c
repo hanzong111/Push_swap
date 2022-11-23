a29ffdbc81a97c2bf69c80defe9cac27fca2e659
Ong Jing Hanz <hanzong111@gmail.com>
2022-11-24 01:54:20 +0800

    Almost done sorting process but soem error handling when smallest number is not at the top

--- ps_sort_for_100.c
+++ ps_sort_for_100.c
@@ -6,30 +6,115 @@
 /*   By: ojing-ha <ojing-ha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/29 22:16:01 by ojing-ha          #+#    #+#             */
-/*   Updated: 2022/11/22 21:17:08 by ojing-ha         ###   ########.fr       */
+/*   Updated: 2022/11/24 01:50:54 by ojing-ha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-int	ft_100_find_position(t_dlist *a, t_dlist *b)
+int	check_if_smallest(int smallest, int content)
 {
-	int	position;
+	if (smallest == content)
+		return (1);
+	else
+		return (0);
+}
+int	get_smallest(t_data *data)
+{
+	t_dlist	*a;
 
-	position = 1;
-	while (b->content > a->content && a->next != NULL)
+	data->chunk.small = data->a;
+	a = data->a->next;
+	while (a->next != NULL)
 	{
-		position++;
+		if (a->content < data->chunk.small->content)
+			data->chunk.small = a;
 		a = a->next;
 	}
-	if (a->next == NULL && b->content > a->content)
-		position = LAST;
-	return (position);
+	if (a->content < data->chunk.small->content)
+			data->chunk.small = a;
+	return (data->chunk.small->content);
+}
+
+int	ft_100_find_position(t_data *data)
+{
+	t_dlist	*a;
+	int		b;
+	int		smallest;
+
+	a = data->a;
+	b = data->b->content;
+	smallest = get_smallest(data);
+	printf("smallest is %d\n", smallest);
+	while (a->next != NULL)
+	{
+		if (b > a->content && b < a->next->content)
+			return (a->index + 1);
+		else if (check_if_smallest(smallest, a->next->content))
+			return (a->index + 2);
+		a = a->next;
+	}
+	printf("lst len is %d\n", lst_len(data->a));
+	if (a->index + 1 == lst_len(data->a))
+		return (-2);
+	return (-1);
 }
 
 void	sort_for_100(t_data *data)
 {
+	int	position;
+
 	data->chunk.counter = data->total_int;
 	label_order(data);
-	push_to_b(data); 
+	push_to_b(data);
+	sort_for_5(data);
+	position = ft_100_find_position(data);
+	printf("position is %d\n", position);
+	if (position == -2)
+	{
+		ft_pa(data);
+		ft_ra(data);
+	}
+	else
+	{
+		ft_repeat_op(data, ft_ra, position);
+		ft_pa(data);
+	}
+	position = ft_100_find_position(data);
+	printf("position is %d\n", position);
+	if (position == -2)
+	{
+		ft_pa(data);
+		ft_ra(data);
+	}
+	else
+	{
+		ft_repeat_op(data, ft_ra, position);
+		ft_pa(data);
+	}
+	position = ft_100_find_position(data);
+	printf("position is %d\n", position);
+	if (position == -2)
+	{
+		ft_pa(data);
+		ft_ra(data);
+	}
+	else
+	{
+		ft_repeat_op(data, ft_ra, position);
+		ft_pa(data);
+	}
+	position = ft_100_find_position(data);
+	printf("position is %d\n", position);
+	if (position == -2)
+	{
+		ft_pa(data);
+		ft_ra(data);
+	}
+	else
+	{
+		ft_repeat_op(data, ft_ra, position);
+		ft_pa(data);
+	}
+
 }
